6e5d31efd66cd4fed639efd8f711112f808563d2
Sainath Balaji Kalyankar <sainathbkalyankar007@gmail.com>
2025-10-17 20:05:39 +0530

    Initial commit - Lost and Found PHP project

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+// Copy this file to includes/config.php and update the values. Do NOT commit includes/config.php to version control.
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = 'Kalyankar@007';
+$db_name = 'lost_and_found';
+
+?>
