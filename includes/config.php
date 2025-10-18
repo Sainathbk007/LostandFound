4c7b2e28467cadc9797a9e397bbd744283a71b7c
Sainath Balaji Kalyankar <106676398+Sainathbk007@users.noreply.github.com>
2025-10-18 18:43:08 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 // Copy this file to includes/config.php and update the values. Do NOT commit includes/config.php to version control.
 $db_host = 'localhost';
 $db_user = 'root';
-$db_pass = 'Kalyankar@007';
+$db_pass = '';
 $db_name = 'lost_and_found';
 
 ?>
